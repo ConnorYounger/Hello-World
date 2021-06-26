45c8aa3b03946be542e0be6f3635246389342a00
cameron-kent-sae <1020666@student.sae.edu.au>
2021-06-26 13:35:36 +1000

    Camera Work

---
+++ Hello World/Assets/Scripts/MenuCameraSwitcher.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraSwitcher : MonoBehaviour
+{
+    [Header("Main Camera")]
+    public GameObject mainCam;
+
+    [Header("Menu Cameras")]
+    public GameObject shelfCam;
+    public GameObject cribCam;
+    public GameObject toyboxCam;
+    public GameObject changetableCam;
+    public GameObject playmatCam;
+
+    private void Update()
+    {
+        
+    }
+
+}
