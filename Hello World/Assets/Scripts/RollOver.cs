2f86202fbe8cb404c43b600cb25c01230ac809af
Samantha <samanthagreig29@hotmail.com>
2021-06-27 19:27:47 +1000

    Using Keys for Roll Over

--- Hello World/Assets/Scripts/RollOver.cs
+++ Hello World/Assets/Scripts/RollOver.cs
@@ -4,13 +4,77 @@ using UnityEngine;
 
 public class RollOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+    private MiniGameInputs controls;
+
+    private bool firstPress = false;
+    private bool secondPress = false;
+    private bool thirdPress = false;
+    private bool fourthPress = false;
+    private bool finalPress = false;
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        controls = new MiniGameInputs();
+        SetInputActions();
     }
 
-    // Update is called once per frame
+    void SetInputActions()
+    {
+        controls.RollOver.Key1.performed += ctx => FirstSwing();
+        controls.RollOver.Key2.performed += ctx => SecondSwing();
+        controls.RollOver.Key3.performed += ctx => ThirdSwing();
+        controls.RollOver.Key4.performed += ctx => FourthSwing();
+        controls.RollOver.Key5.performed += ctx => FullRoll();
+    }
+
+    void FirstSwing()
+    {
+        anim.SetBool("firstPress", true);
+        firstPress = true;
+    }
+
+    void SecondSwing()
+    {
+        if(firstPress == true)
+        {
+            anim.SetBool("firstPress", false);
+            anim.SetBool("secondPress", true);
+            secondPress = true;
+        }
+    }
+
+    void ThirdSwing()
+    {
+        if (secondPress == true)
+        {
+            anim.SetBool("secondPress", false);
+            anim.SetBool("thirdPress", true);
+            thirdPress = true;
+        }
+    }
+
+    void FourthSwing()
+    {
+        if (thirdPress == true)
+        {
+            anim.SetBool("thirdPress", false);
+            anim.SetBool("fourthPress", true);
+            fourthPress = true;
+        }
+    }
+
+    void FullRoll()
+    {
+        if (fourthPress == true)
+        {
+            anim.SetBool("fourthPress", false);
+            anim.SetBool("finalPress", true);
+            finalPress = true;
+        }
+    }
+
     void Update()
     {
         
