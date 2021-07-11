be02e427a8ff9f1ffe9f75fc2cde39a06a2ede88
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-07-11 16:28:46 +1000

    Last min updates

--- Hello World/Assets/Scripts/SimonSays.cs
+++ Hello World/Assets/Scripts/SimonSays.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.InputSystem;
@@ -189,6 +190,7 @@ public class SimonSays : MonoBehaviour
 
         uIElements.SetActive(false);
         winUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("MainMenuButton"));
     }
 
     void CalculateInput()
--- Hello World/Assets/Scripts/Workout.cs
+++ Hello World/Assets/Scripts/Workout.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -327,12 +328,16 @@ public class Workout : MonoBehaviour
     {
         winScreen.SetActive(true);
 
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("WinButton"));
+
         gameFinished = true;
     }
 
     void Lose()
     {
         loseScreen.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(GameObject.Find("LoseButton"));
 
         gameFinished = true;
     }
