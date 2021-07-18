97afd150e39c9dd4fb4e95044ed1a85bc93e5fc6
cameron-kent-sae <1020666@student.sae.edu.au>
2021-07-18 23:06:44 +1000

    Menu refinement

--- Hello World/Assets/Scripts/JitterActivator.cs
+++ Hello World/Assets/Scripts/JitterActivator.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro.Examples;
+using UnityEngine.UI;
 
 public class JitterActivator : MonoBehaviour
 {
     public VertexJitter jitter;
- 
-    public void Update()
+
+    public void FixedUpdate()
     {
         if (EventSystem.current.currentSelectedGameObject == gameObject)
         {
             jitter.enabled = true;
         }
-        if (EventSystem.current.currentSelectedGameObject == gameObject)
+        if (EventSystem.current.currentSelectedGameObject != gameObject && jitter.enabled == true)
         {
             jitter.enabled = false;
         }
