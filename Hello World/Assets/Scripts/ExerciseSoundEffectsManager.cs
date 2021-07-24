5474b6f48b84d1b452cd59edc74d882659241e88
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-07-24 14:11:36 +1000

    Exercise Sound Effects Manager

--- Hello World/Assets/Scripts/ExerciseSoundEffectsManager.cs
+++ Hello World/Assets/Scripts/ExerciseSoundEffectsManager.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class ExerciseSoundEffectsManager : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    [Header("Sounds")]
+    public AudioClip[] sucessSounds;
+    public AudioClip[] failSounds;
+    public AudioClip winSound;
+    public AudioClip loseSound;
+
+    public AudioClip[] babyCrySounds;
+
+    public bool randomisSoundCollections;
+    private int currentSucessSound;
+    private int currentFailSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -15,4 +29,73 @@ public class ExerciseSoundEffectsManager : MonoBehaviour
     {
         
     }
+
+    public void PlaySucessSound()
+    {
+        if (randomisSoundCollections)
+        {
+            int rand = Random.Range(0, sucessSounds.Length);
+
+            PlaySound(sucessSounds[rand]);
+        }
+        else
+        {
+            PlaySound(sucessSounds[currentSucessSound]);
+
+            currentSucessSound++;
+
+            if (currentSucessSound >= sucessSounds.Length)
+                currentSucessSound = 0;
+        }
+    }
+
+    public void PlayFailSound()
+    {
+        if (randomisSoundCollections)
+        {
+            int rand = Random.Range(0, failSounds.Length);
+
+            PlaySound(failSounds[rand]);
+        }
+        else
+        {
+            PlaySound(failSounds[currentFailSound]);
+
+            currentFailSound++;
+
+            if (currentFailSound >= failSounds.Length)
+                currentFailSound = 0;
+        }
+    }
+
+    public void PlayWinSound()
+    {
+        PlaySound(winSound);
+    }
+
+    public void PlayLoseSound()
+    {
+        PlaySound(loseSound);
+    }
+
+    public void PlayBabyCrySound()
+    {
+        int rand = Random.Range(0, babyCrySounds.Length);
+
+
+    }
+
+    void PlaySound(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    void PlaySound(AudioClip audioClip, bool loop)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        audioSource.loop = loop;
+    }
+
 }
