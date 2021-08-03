924ebb84fcb53c64eef173f3ab03d3a296db7b0a
cameron-kent-sae <1020666@student.sae.edu.au>
2021-08-03 14:22:31 +1000

    Audio mixer implementing

--- Hello World/Assets/Scripts/MenuAudioManager.cs
+++ Hello World/Assets/Scripts/MenuAudioManager.cs
@@ -6,39 +6,40 @@ using UnityEngine.UI;
 
 public class MenuAudioManager : MonoBehaviour
 {
-    [Header("Cam Movements")]
-    public Button camShelf;
-    public Button camToybox;
-    public Button camCrib;
-    public Button camMat;
-    public Button camChanget;
-    public Button camDoor;
+    
+    //Header("Cam Movements")]
+    //public Button camShelf;
+    //public Button camToybox;
+    //public Button camCrib;
+    //public Button camMat;
+    //public Button camChanget;
+    //public Button camDoor;
 
-    [Header("UI Buttons")]
-    public Button OptionChange;
-    public Button OptionSelect;
-    public Button Back;
+    //[Header("UI Buttons")]
+    //public Button OptionChange;
+    //public Button OptionSelect;
+    //public Button Back;
 
 
-    [Header("Audio Clips")]
+    //[Header("Audio Clips")]
     //Camera Movement Clips//
-    public AudioClip clipShelf;
-    public AudioClip clipToybox;
-    public AudioClip clipCrib;
-    public AudioClip clipMat;
-    public AudioClip clipChanget;
-    public AudioClip clipDoor;
+    //public AudioClip clipShelf;
+    //public AudioClip clipToybox;
+    //public AudioClip clipCrib;
+    //public AudioClip clipMat;
+    //public AudioClip clipChanget;
+    //public AudioClip clipDoor;
     //Button Selection Clips//
-    public AudioClip clipOptionChange;
-    public AudioClip clipOptionSelect;
-    public AudioClip clipBack;
-
+    //public AudioClip clipOptionChange;
+    //public AudioClip clipOptionSelect;
+    //public AudioClip clipBack;
+    
     private AudioSource audioSource;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        camToybox.onClick.AddListener(delegate { PlayAudioClip(clipShelf); });
+        //camToybox.onClick.AddListener(delegate { PlayAudioClip(clipShelf); });
     }
 
     public void PlayAudioClip(AudioClip clip)
